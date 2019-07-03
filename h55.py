8855bd097e288ffde54bf97fb9f788c0271f5a13
BrahminiA <43632503+BrahminiA@users.noreply.github.com>
2019-07-03 14:10:35 +0530

    Add files via upload

---
+++ h55.py
@@ -0,0 +1,17 @@
+k,n =input().split()
+n=int(n)
+arr=map(int,input().split())
+arr=list(arr)
+for i in range(0, n):       
+    first = arr[0];
+    for j in range(0, len(arr)-1):       
+        arr[j] = arr[j+1];       
+    arr[len(arr)-1] = first;
+for j in range(0,len(arr)):
+    if(j==((len(arr)-1))):
+        print(arr[j])
+    else:
+       print(arr[j],end=" ")
+print(".")
+
+
